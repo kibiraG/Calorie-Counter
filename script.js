812a30edf4d71ac94e3af873d13504e194054410
kibiraG <kibiragjunior@gmail.com>
2024-03-16 13:38:58 +0300

    third commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+
+/*-In programming, it is standard practice to practice to prefix a variable 
+    with is or has to indicate that the variable is boolean. 
+-Using let so as to reassign a variable*/
+
+let isError = false;
+
+//Function to clean the string value and ensure you have a value
+// The split() method splits a string into an array of substrings and returns the new array
+
+function cleanInputString(str) {
+
+}
